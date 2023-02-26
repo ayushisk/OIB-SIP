84d11ac4d82dc37900bb4c2011562b2990ca2fe4
Ayushi Sinha <111056313+ayushisk@users.noreply.github.com>
2023-02-27 01:09:06 +0530

    Update script.js

--- todo/script.js
+++ todo/script.js
@@ -5,8 +5,9 @@ window.addEventListener('load', () => {
 
 	form.addEventListener('submit', (e) => {
 		e.preventDefault();
+		
 
-		const task = input.value;
+		const task = input.value; 
 
 		const task_el = document.createElement('div');
 		task_el.classList.add('task');
@@ -34,25 +35,44 @@ window.addEventListener('load', () => {
 		const task_delete_el = document.createElement('button');
 		task_delete_el.classList.add('delete');
 		task_delete_el.innerText = 'Delete';
+		
+		if(task.trim()!=0)
+			{
+
+			
+				if (task_edit_el.innerText.toLowerCase() == "edit") {
+					task_edit_el.innerText = "Save";
+					task_input_el.removeAttribute("readonly");
+					task_input_el.focus();
+				} 	
+				else {
+						task_edit_el.innerText = "Edit";
+						task_input_el.setAttribute("readonly", "readonly");
+					}
+		    }
+			else
+			{
+				alert("PLEASE ENTER YOUR TASK");
+				return false;
+
+			}
+			
 
 		task_actions_el.appendChild(task_edit_el);
 		task_actions_el.appendChild(task_delete_el);
 
+		
 		task_el.appendChild(task_actions_el);
-
+		
 		list_el.appendChild(task_el);
-
+		
+		
 		input.value = '';
 
 		task_edit_el.addEventListener('click', (e) => {
-			if (task_edit_el.innerText.toLowerCase() == "edit") {
-				task_edit_el.innerText = "Save";
-				task_input_el.removeAttribute("readonly");
-				task_input_el.focus();
-			} else {
-				task_edit_el.innerText = "Edit";
-				task_input_el.setAttribute("readonly", "readonly");
-			}
+			
+			
+
 		});
 
 		task_delete_el.addEventListener('click', (e) => {
